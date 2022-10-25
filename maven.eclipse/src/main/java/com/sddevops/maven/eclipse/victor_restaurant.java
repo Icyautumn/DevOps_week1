b9dd0a846b5cd438be41f13cb3f6ef2f383512b2
Icyautumn <victorchua865@gmail.com>
2022-10-25 15:09:53 +0800

    added a victor_restaurant class that stores id, name and address

---
+++ maven.eclipse/src/main/java/com/sddevops/maven/eclipse/victor_restaurant.java
@@ -0,0 +1,12 @@
+package com.sddevops.maven.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class victor_restaurant
+{
+    private int id;
+    private string name;
+    private address address;
+}
